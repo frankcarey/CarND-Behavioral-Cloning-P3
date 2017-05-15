8a657d3a7ab2ebdb79c9379be62eed26f0fee88e
Frank Carey <fcarey@gmail.com>
2017-05-15 00:53:37 -0400

    This one does pretty well, getting across the bridge.

--- train.py
+++ train.py
@@ -4,10 +4,18 @@ import utils
 
 # TUnable Parameters
 batch_size = 2048
-epochs = 2
-l2_reg = 0.001
-cam_adj = 0.5
-X, y = utils.get_data('./data/train_1', cam_adj)
+epochs = 10
+learning_rate = 0.0001
+l2_reg = 0.0001
+cam_adj = 1.0
+nb_derivatives = 2
+#train_folder = './data/example_training'
+train_folder = './data/train_2'
+
+
+
+
+X, y = utils.get_data(train_folder, cam_adj)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -16,30 +24,32 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.noise import GaussianNoise
-from keras import regularizers
+from keras import regularizers, optimizers
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((50, 20), (0,0))))
 
-model.add(Conv2D(24, (5, 5), strides=(2, 2),  activation="elu", kernel_regularizer=regularizers.l2(0.01)))
-#model.add(Conv2D(36, (5, 5), strides=(2, 2),  activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
-#model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
-#model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
-#model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Conv2D(24, (5, 5), strides=(2, 2),  padding='same', activation="relu", kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), padding='same', activation="relu", kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), padding='same', activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
+#model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
 
 #model.add(MaxPooling2D())
 model.add(Flatten())
+#model.add(Dropout(0.25))
 model.add(Dense(100, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
+#model.add(Dropout(0.25))
 model.add(Dense(50, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
 model.add(Dense(1, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
 
-train_steps = math.ceil(len(X_train*2)/batch_size)
-valid_steps = math.ceil(len(X_test*2)/batch_size)
+train_steps = math.ceil(len(X_train)*nb_derivatives/batch_size)
+valid_steps = math.ceil(len(X_test)*nb_derivatives/batch_size)
 
 print(train_steps)
 
-model.compile(loss='mse', optimizer="adam")
+adam = optimizers.Adam(lr=0.001)
+model.compile(loss='mse', optimizer=adam)
 model.fit_generator(
         utils.data_generator(X_train, y_train),
         steps_per_epoch=train_steps,
--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def get_data(folder='./data/train_1', cam_adj=0.3):
                measurement -= cam_adj
             # if camera != 0:
             #     break
-            print(camera, measurement)
+            #print(camera, measurement)
             measurements.append(measurement)
             images_src.append(current_path)
     nb_training_images = len(images_src)
@@ -52,7 +52,7 @@ def data_generator(X, y, batch_size=32, crop=None, normalize=None):
         nb_images = len(X)
         # iterate over the total number of batches necessary.
         total_batches = math.ceil(nb_images / batch_size)
-        derivatives = 1
+        derivatives = 2
         for batch_i in range(total_batches):
             start = batch_i*batch_size
             #print("Start", start)
@@ -74,7 +74,7 @@ def data_generator(X, y, batch_size=32, crop=None, normalize=None):
             for j, filename in enumerate(batch_items):
                 orig_idx = start+j
                 #print("orig_idx", orig_idx)
-                #flip_idx = j + nb_batch_items
+                flip_idx = j + nb_batch_items
                 img = cv2.imread(filename)
                 if crop:
                     start_y = crop[0][0]
@@ -84,10 +84,10 @@ def data_generator(X, y, batch_size=32, crop=None, normalize=None):
                     #print(start_y, stop_y, start_x, stop_x)
                     img = img[start_y:stop_y, start_x: stop_x]
                 if normalize:
-                    img = img / 255. 
+                    img = img / 255.
                 batch_features[j] = img
                 batch_labels[j] = y[orig_idx]
-                #batch_features[flip_idx] = cv2.flip(batch_features[j], 1)
-                #batch_labels[flip_idx] = batch_labels[j]*-1.0
+                batch_features[flip_idx] = cv2.flip(batch_features[j], 1)
+                batch_labels[flip_idx] = batch_labels[j]*-1.0
             #print("yield")
             yield (batch_features, batch_labels)
