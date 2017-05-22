331f8f4cc411836f0018378303d6e5d012f926ca
Frank Carey <fcarey@gmail.com>
2017-05-22 01:16:56 -0400

    WIP: Fixed some bugs.

--- train.py
+++ train.py
@@ -3,21 +3,27 @@ import math
 import utils
 
 # TUnable Parameters
-batch_size = 2048
-epochs = 10
-learning_rate = 0.0001
-l2_reg = 0.0001
-cam_adj = 1.0
-nb_derivatives = 2
+batch_size = 256 #2048
+epochs = 2 #20
+learning_rate = None #0.0005
+l2_reg = 0.0
+cam_adj = 0.10
+# nb_derivatives = 2
 #train_folder = './data/example_training'
-train_folder = './data/train_2'
-
-
-
+train_folder = './data/example_training'
+act="elu"
+pad="valid"
 
 X, y = utils.get_data(train_folder, cam_adj)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+print("Source training samples:", len(X_train), len(y_train) )
+print("Source validation samples:", len(X_test), len(y_test))
+
+train_steps = math.ceil(len(X_train)/batch_size)
+valid_steps = math.ceil(len(X_test)/batch_size)
+print("train_steps", train_steps)
+print("valid_steps", valid_steps)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
@@ -27,33 +33,42 @@ from keras.layers.noise import GaussianNoise
 from keras import regularizers, optimizers
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 127.5 -1, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((50, 20), (0,0))))
-
-model.add(Conv2D(24, (5, 5), strides=(2, 2),  padding='same', activation="relu", kernel_regularizer=regularizers.l2(l2_reg)))
-model.add(Conv2D(36, (5, 5), strides=(2, 2), padding='same', activation="relu", kernel_regularizer=regularizers.l2(l2_reg)))
-model.add(Conv2D(48, (5, 5), strides=(2, 2), padding='same', activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
-#model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation="elu", kernel_regularizer=regularizers.l2(l2_reg)))
+#model.add(GaussianNoise(0.1))
+model.add(Conv2D(24, 5, strides=(2, 2), padding=pad, activation=act, kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Dropout(0.25))
+model.add(Conv2D(36, 5, strides=(2, 2), padding=pad, activation=act, kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Dropout(0.25))
+model.add(Conv2D(48, 5, strides=(2, 2), padding=pad, activation=act, kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, 3, strides=(1, 1), padding=pad, activation=act, kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, 3, strides=(1, 1), padding=pad, activation=act, kernel_regularizer=regularizers.l2(l2_reg)))
+model.add(Dropout(0.25))
 
 #model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dropout(0.25))
+model.add(Dense(100, kernel_regularizer=regularizers.l2(l2_reg), activation=act))
+model.add(Dropout(0.25))
+model.add(Dense(50, kernel_regularizer=regularizers.l2(l2_reg), activation=act))
+model.add(Dropout(0.25))
+model.add(Dense(10, kernel_regularizer=regularizers.l2(l2_reg), activation=act))
 #model.add(Dropout(0.25))
-model.add(Dense(100, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
-#model.add(Dropout(0.25))
-model.add(Dense(50, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
-model.add(Dense(1, kernel_regularizer=regularizers.l2(l2_reg), activation='elu'))
+#model.add(GaussianNoise(0.1))
+model.add(Dense(1, kernel_regularizer=regularizers.l2(l2_reg), activation=act))
 
-train_steps = math.ceil(len(X_train)*nb_derivatives/batch_size)
-valid_steps = math.ceil(len(X_test)*nb_derivatives/batch_size)
 
-print(train_steps)
 
-adam = optimizers.Adam(lr=0.001)
-model.compile(loss='mse', optimizer=adam)
+# adam = optimizers.Adam(
+#     #lr=learning_rate
+# )
+model.compile(loss='mse', optimizer='adam')
 model.fit_generator(
-        utils.data_generator(X_train, y_train),
+        utils.data_generator(X_train, y_train, batch_size=batch_size),
         steps_per_epoch=train_steps,
-        validation_data=utils.data_generator(X_test, y_test),
+        validation_data=utils.data_generator(X_test, y_test, batch_size=batch_size),
         validation_steps=valid_steps,
         epochs=epochs
 )
--- utils.py
+++ utils.py
@@ -4,8 +4,16 @@ import numpy as np
 from sys import getsizeof
 import math
 
-def get_data(folder='./data/train_1', cam_adj=0.3):
+def shuffle_sync(a, b):
+    rng_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(rng_state)
+    np.random.shuffle(b)
+
+def get_data(folder, cam_adj):
     lines = []
+
+    print("Loading training folder:", folder)
 
     with open(folder +'/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
@@ -19,28 +27,49 @@ def get_data(folder='./data/train_1', cam_adj=0.3):
 
     #skip the first header line.
     for line in lines[1:]:
-        for camera in range(3):
-            source_path = line[camera]
-            filename = source_path.split('/')[-1]
-            current_path = folder +'/IMG/' + filename
-            measurement = float(line[3])
-            if camera == 1:
-               measurement += cam_adj
-            if camera == 2:
-               measurement -= cam_adj
-            # if camera != 0:
-            #     break
-            #print(camera, measurement)
-            measurements.append(measurement)
-            images_src.append(current_path)
+        measurement = float(line[3])
+        prob = np.random.random()
+        camera = 0
+        if prob < 0.7 and (-0.01 < measurement or measurement < 0.01):
+            # drop 0 angle data with 70% prbability:
+            camera = np.random.choice(['left', 'right'])
+                #for camera in range(3):
+
+
+            if camera == "left":
+                #print("left")
+                camera = 1
+                measurement += cam_adj
+            if camera == "right":
+                #print("right")
+                camera = 2
+                measurement -= cam_adj
+            #if camera == 0:
+                #print("center")
+        source_path = line[camera]
+        filename = source_path.split('/')[-1]
+        current_path = folder +'/IMG/' + filename
+        measurements.append(measurement)
+        images_src.append(current_path)
     nb_training_images = len(images_src)
-    print("training images:", nb_training_images, getsizeof(images_src))
+    print("training images:", nb_training_images)
     return images_src, measurements
 
-def data_generator(X, y, batch_size=32, crop=None, normalize=None):
+def data_generator(X, y, batch_size=128, crop=None, normalize=None):
     #f = h5py.File("/tmp/testData.hdf5", "w")
     #dset = f.create_dataset("X_train", (160, 320, 3), dtype='float32')
 
+    nb_images = len(X)
+    print("Samples to batch:", nb_images)
+    # iterate over the total number of batches necessary.
+    total_batches = math.ceil(nb_images / batch_size)
+    print("necessary_batches:", total_batches)
+    #derivatives = 2
+
+    height = 160
+    width = 320
+    batch_features = np.zeros((batch_size*2, height, width, 3))
+    batch_labels = np.zeros((batch_size*2, 1))
 
     while 1:
         # augmented_images, augmented_measurements = [], []
@@ -49,45 +78,44 @@ def data_generator(X, y, batch_size=32, crop=None, normalize=None):
         #     augmented_measurements.append(measurement)
         #     #augmented_images.append(cv2.flip(image,1))
         #     #augmented_measurements.append(measurement*-1.0)
-        nb_images = len(X)
-        # iterate over the total number of batches necessary.
-        total_batches = math.ceil(nb_images / batch_size)
-        derivatives = 2
+        shuffle_sync(X, y)
         for batch_i in range(total_batches):
             start = batch_i*batch_size
-            #print("Start", start)
             end = start+batch_size
             #print('end', end)
-            batch_items = X[start:end]
-            nb_batch_items = len(batch_items)
+            batch_X = X[start:end]
+            batch_y = y[start:end]
+            nb_batch_items = len(batch_X)
+            # print("Starting batch:", batch_i)
+            # print(".. Attempting From: {} To: {}".format(start, end))
+            # print(".. Found items:", len(batch_X), len(batch_y))
 
-            height = 160
-            width = 320
-            if crop:
-                height -= sum(crop[0])
-                width -= sum(crop[1])
-            batch_features = np.zeros((nb_batch_items*derivatives, height, width, 3))
-            batch_labels = np.zeros((nb_batch_items*derivatives,1))
 
+            # if crop:
+            #     height -= sum(crop[0])
+            #     width -= sum(crop[1])
             # For every item in the source return the item and another item that's flipped horizontally,
             # and a weight that's flipped.
-            for j, filename in enumerate(batch_items):
-                orig_idx = start+j
+            # print("Processing batch items..")
+            for j, (filename, measurement) in enumerate(zip(batch_X, batch_y)):
+                # print("item:", j, measurement)
+
+                #orig_idx = start+j
                 #print("orig_idx", orig_idx)
-                flip_idx = j + nb_batch_items
+                #flip_idx = j + nb_batch_items
                 img = cv2.imread(filename)
-                if crop:
-                    start_y = crop[0][0]
-                    stop_y = height + start_y
-                    start_x = crop[1][0]
-                    stop_x = width + start_x
-                    #print(start_y, stop_y, start_x, stop_x)
-                    img = img[start_y:stop_y, start_x: stop_x]
-                if normalize:
-                    img = img / 255.
-                batch_features[j] = img
-                batch_labels[j] = y[orig_idx]
-                batch_features[flip_idx] = cv2.flip(batch_features[j], 1)
-                batch_labels[flip_idx] = batch_labels[j]*-1.0
+                # if crop:
+                #     start_y = crop[0][0]
+                #     stop_y = height + start_y
+                #     start_x = crop[1][0]
+                #     stop_x = width + start_x
+                #     #print(start_y, stop_y, start_x, stop_x)
+                #     img = img[start_y:stop_y, start_x: stop_x]
+               #if normalize:
+                #    img = img / 255.
+                batch_features[(j*2)] = img
+                batch_labels[(j*2)] = measurement
+                batch_features[(j*2)+1] = cv2.flip(img, 1)
+                batch_labels[(j*2)+1] = measurement*-1.0
             #print("yield")
-            yield (batch_features, batch_labels)
+            yield ((batch_features, batch_labels))
